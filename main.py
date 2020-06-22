108a490bc74821f87d15075579f872e817cee842
fulviodenza <fulviodenza@hotmail.it>
2020-06-22 20:33:28 +0200

    finish main.py and init manage.sh to start the script at the boot

--- main.py
+++ main.py
@@ -1,3 +1,32 @@
 import notify2
+import json
+import urllib.request
+import numpy
 
-url = "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-andamento-nazionale-latest.json"
+url_today = urllib.request.urlopen(
+    "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-andamento-nazionale-latest.json")
+url_yesterday = urllib.request.urlopen(
+    "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-andamento-nazionale.json"
+)
+
+data_today = json.loads(url_today.read())
+data_yesterday = json.loads(url_yesterday.read())
+yesterday_index = len(data_yesterday)
+notify2.init("covid-info")
+
+variazione_deceduti = int(data_today[0]["deceduti"]) - int(data_yesterday[yesterday_index-2]["deceduti"])
+variazione_guariti = int(data_today[0]["dimessi_guariti"]) - int(data_yesterday[yesterday_index-2]["dimessi_guariti"])
+print(variazione_deceduti)
+msg = "POSITIVI: {} (+{}) di cui {} ({}) attuali DECEDUTI: {} (+{}); GUARITI: {} (+{})".format(
+    data_today[0]["totale_casi"], data_today[0]["nuovi_positivi"], 
+    data_today[0]["totale_positivi"], data_today[0]["variazione_totale_positivi"], 
+    data_today[0]["deceduti"], 
+    variazione_deceduti, data_today[0]["dimessi_guariti"], variazione_guariti
+    )
+
+n = notify2.Notification("Covid today",
+                         msg,
+                         "notification-message-im"   # Icon name
+                        )
+n.timeout = 10000
+n.show()
---
+++ manage.sh
@@ -0,0 +1,3 @@
+#!bin/sh
+sudo cp -i main.py /bin
+python main.py
