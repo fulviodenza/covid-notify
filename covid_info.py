497a0f7b80b4a8c6bfa74b46fd17bd7e325ec670
fulviodenza <fulviodenza@hotmail.it>
2020-06-23 11:27:20 +0200

    repair scheduler

--- covid_info.py
+++ covid_info.py
@@ -4,31 +4,39 @@ except ImportError:
     print("run pip install notify2")
 import json
 import urllib.request
+from apscheduler.schedulers.blocking import BlockingScheduler
 
-url_today = urllib.request.urlopen(
-    "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-andamento-nazionale-latest.json")
-url_yesterday = urllib.request.urlopen(
-    "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-andamento-nazionale.json"
-)
+scheduler = BlockingScheduler()
 
-data_today = json.loads(url_today.read())
-data_yesterday = json.loads(url_yesterday.read())
-yesterday_index = len(data_yesterday)
-notify2.init("covid-info")
-
-variazione_deceduti = int(data_today[0]["deceduti"]) - int(data_yesterday[yesterday_index-2]["deceduti"])
-variazione_guariti = int(data_today[0]["dimessi_guariti"]) - int(data_yesterday[yesterday_index-2]["dimessi_guariti"])
-
-msg = "POSITIVI: {} (+{}) di cui {} ({}) attuali DECEDUTI: {} (+{}); GUARITI: {} (+{})".format(
-    data_today[0]["totale_casi"], data_today[0]["nuovi_positivi"], 
-    data_today[0]["totale_positivi"], data_today[0]["variazione_totale_positivi"], 
-    data_today[0]["deceduti"], 
-    variazione_deceduti, data_today[0]["dimessi_guariti"], variazione_guariti
+def notify():
+    url_today = urllib.request.urlopen(
+        "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-andamento-nazionale-latest.json")
+    url_yesterday = urllib.request.urlopen(
+        "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-andamento-nazionale.json"
     )
 
-n = notify2.Notification("Covid today",
-                         msg,
-                         "notification-message-im"   # Icon name
-                        )
-n.timeout = 10000
-n.show()
+    data_today = json.loads(url_today.read())
+    data_yesterday = json.loads(url_yesterday.read())
+    yesterday_index = len(data_yesterday)
+    notify2.init("covid-info")
+
+    variazione_deceduti = int(data_today[0]["deceduti"]) - int(data_yesterday[yesterday_index-2]["deceduti"])
+    variazione_guariti = int(data_today[0]["dimessi_guariti"]) - int(data_yesterday[yesterday_index-2]["dimessi_guariti"])
+
+    msg = "POSITIVI: {} (+{}) di cui {} ({}) attuali DECEDUTI: {} (+{}); GUARITI: {} (+{})".format(
+        data_today[0]["totale_casi"], data_today[0]["nuovi_positivi"], 
+        data_today[0]["totale_positivi"], data_today[0]["variazione_totale_positivi"], 
+        data_today[0]["deceduti"], 
+        variazione_deceduti, data_today[0]["dimessi_guariti"], variazione_guariti
+        )
+
+    n = notify2.Notification("Covid today",
+                            msg,
+                            "notification-message-im"   # Icon name
+                            )
+    n.timeout = 10000
+    n.show()
+
+
+scheduler.add_job(notify, 'interval', hours=1)
+scheduler.start()
--- manage.sh
+++ manage.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/bash
-sudo echo "00 18 * * *   $(pwd)/covid_info.py" >> /var/spool/cron/$USER
-python ~/covid_info.py
+cp covid_info.py ~
+sudo chmod +x covid_info.py
+nohup python ~/covid_info.py &
